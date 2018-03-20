60b3f8ccf1b893e118c1be388a3cde41e3b5aaa7
hayani said <hayanisaid1995@gmail.com>
2018-03-20 17:02:32 +0000

    add remote and push code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,MenuController,Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	distance: number = 2;
+	dualValue2: any = { lower: 33, upper: 60 };
+	text: number = 0;
+	gender:string="Women";
+	showme:boolean=true;
+	lang : string="English";
+	headrName:string="Tinder";
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+  	public navCtrl: NavController,
+  	public menuCtrl: MenuController,
+  	public platform:Platform)
+  	 {
+        this.menuCtrl.enable(true)
+        if(this.platform.is('ios')){
+        	this.headrName="Profile";
+        }
+        
+     }
 
+  chekPlatform() {
+   console.log('hello there')
   }
-
 }
